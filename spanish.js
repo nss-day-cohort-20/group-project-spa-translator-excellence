bcfb1ecce01c488f0b8c5cc46af624f1e79ba798
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-14 16:10:14 -0500

    work on Bobby's Language Object

---
+++ spanish.js
@@ -0,0 +1,32 @@
+console.log("ahoy, captain.");
+
+// for your javascript: the translate button id = translateBtn
+// the dropdown to select your languate id = langSelect
+// the dropdown option for your language id = your language (ie id = german)
+// the element you will be displaying the output in id = translateArea
+// the user input text area id = userInput
+// our main variable = finalMessage
+
+// to test if your language was selected:
+// var langSelected = document.getElementById("selectedLang")
+// 	if (langSelected.options[langSelected.selectedIndex].value === "spanish") {
+// 		return spanish
+// 	};
+
+
+// var finalMessage = (function(oldFinalMessage) {
+	var spanishWords = {
+		Merry: "Feliz", 
+		Christmas: "Navidad", 
+		and: "y", 
+		Happy: "Prospero",
+		New: "Ano",
+		Year: "Felizidad"
+	}
+	console.log("Spanish word for 'and': ", spanishWords.and);
+
+	
+
+// var langSelected = document.getElementById("selectedLang");
+
+// if (langSelected.options[langSelected.selectedIndex].value === "spanish")
