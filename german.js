a095f5e6aba3e3c0c25bad8bf1a912e61758fb06
patrickleon <leon.patrick19@gmail.com>
2017-06-15 11:44:31 -0500

    added more words

--- german.js
+++ german.js
@@ -2,12 +2,26 @@ var finalMessage = (function (oldMessage) {
 	oldMessage.translateGWord = function(){
 
 	let myLexicon = {
-	hello: "hallo", 
-	bye: "Nebensache", 
-	blue:"blau", 
-	red: "reduziert", 
-	good_morning:"guten morgen",
-	good_evening:"guten abend"
+	"hello": "hallo", 
+	"bye": "Nebensache", 
+	"blue":"blau", 
+	"red": "reduziert", 
+	"good morning":"guten morgen",
+	"good afternoon": "guten nachmittag",
+	"good evening":"guten abend",
+	"the": "das",
+	"house": "hause",
+	"work": "arbeit",
+	"thank you" "danke",
+	"thank you very much" "vielen dank",
+	"you're welcome": "bitteschön",
+	"please": "bitte",
+	"yes": "ja",
+	"no": "nein" 
+	"Excuse me" "entschuldigen Sie bitte",
+	"Pardon me" "entschuldigung",
+	"I'm sorry" "es tut mir leid",
+
 };
 
 var langSelected = document.getElementById("selectedLang")
