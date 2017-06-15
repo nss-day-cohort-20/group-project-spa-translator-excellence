d4ca0b0cdda3ba0c9bc164eb06b79030e91f6dee
patrickleon <leon.patrick19@gmail.com>
2017-06-15 09:16:55 -0500

    germanlexicon

---
+++ german.js
@@ -0,0 +1,9 @@
+var myLexicon = {
+	hello: "hallo", 
+	bye: "Nebensache", 
+	blue:"blau", 
+	red: "reduziert", 
+	good_morning:"guten morgen",
+	good_evening:"guten abend"
+}
+console.log ("The German word for hello", myLexicon.red)
